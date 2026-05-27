aa2baf8d6fc957a601097f28d9b1be0f8b26c4c8
tatiana1009-123 <t.ferro1009@gmail.com>
2026-05-27 15:00:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ with open ("quotes.txt") as file:
 def send_email():
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()       #Makes connection secure
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
+        connection.login(user=secrets.MY_EMAIL, password=secrets.MY_PASSWORD)
+        connection.sendmail(from_addr=secrets.MY_EMAIL,
                             to_addrs="tfm606.606@gmail.com",
                             msg=f"Subject: Friendly Reminder\n\n{random_quote}")
         connection.close()
