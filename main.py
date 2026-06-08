bb7544b5d3530d1da58343df1622ddd23544d84a
tatiana1009-123 <t.ferro1009@gmail.com>
2026-06-08 10:44:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
+recipients = ["tfm606.606@gmail.com", "pjmozingo604@gmail.com", "julio_ferro2009@hotmail.com", "maritza_montaa@yahoo.es"]
+
 with open("quotes.txt") as file:
     all_quotes = file.readlines()
     random_quote = random.choice(all_quotes).strip()
@@ -15,6 +17,6 @@ with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.login(user=MY_EMAIL, password=MY_PASSWORD)
     connection.sendmail(
         from_addr=MY_EMAIL,
-        to_addrs="tfm606.606@gmail.com",
+        to_addrs=recipients,
         msg=f"Subject: Friendly Reminder\n\n{random_quote}"
     )
